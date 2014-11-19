e4130059a4073f080ad2e24697a84c1d44560aaa
Markus Kohlhase <mail@markus-kohlhase.de>
2014-11-19 05:12:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# go-modbus
+
+a free [Modbus](http://en.wikipedia.org/wiki/Modbus) library
+for [Go](http://golang.org/)
+
+## License
+
+This library is licensed under the MIT license
---
+++ api.go
@@ -0,0 +1,85 @@
+/**
+ * Copyright (C) 2014, Markus Kohlhase <mail@markus-kohlhase.de>
+ *
+ * Modbus API
+ */
+
+package modbus
+
+/* Modbus Error */
+
+type Error struct {
+
+	// error code
+	code uint8
+
+	// exception code
+	exception uint8
+}
+
+type Master interface {
+
+	/**************
+	 * Bit access *
+	 **************/
+
+	/* Physical Discrete Inputs */
+
+	// Function Code 2
+	ReadDiscreteInputs(address, quantity uint16) (inputs []bool, err error)
+
+	/* Internal Bits Or Physical Coils */
+
+	// Function Code 1
+	ReadCoils(address, quantity uint16) (coils []bool, err error)
+
+	// Function Code 5
+	WriteSingleCoil(address uint16, coil bool) error
+
+	// Function Code 15
+	WriteMultipleCoils(address uint16, coils []bool) error
+
+	/******************
+	 * 16 bits access *
+	 ******************/
+
+	/* Physical Input Registers */
+
+	// Function Code 4
+	ReadInputRegisters(address, quantity uint16) (readRegisters []int16, err error)
+
+	// Function Code 3
+	ReadHoldingRegisters(address, quantity uint16) (readRegisters []int16, err error)
+
+	// Function Code 6
+	WriteSingleRegister(address uint16, value int16) error
+
+	// Function Code 16
+	WriteMultipleRegisters(address, quantity uint16, values []int16) error
+
+	// Function Code 23
+	ReadWriteMultipleRegisters(readAddress, readQuantity, writeAddress, writeQuantity uint16, values []int16) (readValues []int16, err error)
+
+	// Function Code 22
+	MaskWriteRegister(address uint16, andMask, orMask int16) error
+
+	// Function Code 24
+	ReadFifoQueue(address uint16) (count uint16, values []int16, err error)
+
+	/**********************
+	 * File record access *
+	 **********************/
+
+	// TODO: specify methods
+
+	/***************
+	 * Diagnostics *
+	 **************/
+
+	// TODO: specify methods
+
+}
+
+type Slave interface {
+
+}
